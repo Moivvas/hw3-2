1de7b173ba7185c15ee658a85de7e9acbed1d5f0
Moivvas <moivvas@gmail.com>
2023-09-01 13:21:30 +0300

    cleared, tested

--- factorize.py
+++ factorize.py
@@ -1,6 +1,5 @@
 from timeit import timeit
 from multiprocessing import Pool, cpu_count
-import time
 
 
 def factorize(*numbers):
@@ -10,6 +9,7 @@ def factorize(*numbers):
         main_divide_list.append(divide_list)
     return main_divide_list
 
+
 def super_factorize(*numbers):
     main_divide_list = []
     for num in numbers:
@@ -17,11 +17,20 @@ def super_factorize(*numbers):
         main_divide_list.append(divide_list)
     return main_divide_list
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     
-    execution_time = timeit(lambda: factorize(128, 255, 23243503, 52345674), number=1)
-    print(f"Execution time: {execution_time} seconds")
-    
+    execution_time = timeit(
+        lambda: factorize(128, 255, 23243503, 52345674), number=1
+    )
+    print(
+        f"Execution time of factorize: {execution_time} seconds"
+    )
+
     pool = Pool(cpu_count())
-    execution_time_super_factorize = timeit(lambda: pool.map(super_factorize, [128, 255, 23243503, 52345674]), number=1)
-    print(f"Execution time of super_factorize: {execution_time_super_factorize} seconds")
+    execution_time_super_factorize = timeit(
+        lambda: pool.map(super_factorize, [128, 255, 23243503, 52345674]), number=1
+    )
+    print(
+        f"Execution time of super_factorize: {execution_time_super_factorize} seconds"
+    )
